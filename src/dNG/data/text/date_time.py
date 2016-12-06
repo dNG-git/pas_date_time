6d67c7759e441fd31d13288dc48fd77eae002521
NotTheEvilOne <git-NTEO@vplace.de>
2016-12-06 19:53:09 +0100

    Trivial: Code cleanup on pylint2 base

--- src/dNG/data/text/date_time.py
+++ src/dNG/data/text/date_time.py
@@ -17,6 +17,8 @@ https://www.direct-netware.de/redirect?licenses;mpl2
 #echo(__FILEPATH__)#
 """
 
+# pylint: disable=import-error
+
 from math import floor
 from time import gmtime, strftime, time
 
